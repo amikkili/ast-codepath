4fd91bf4bd7434c70cf2d86fbbe5edc28a0180f7
anil.mikkili <anil.mikkili@stacknexus.io>
2026-05-06 12:23:11 -0700

    Fix export path

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  reactStrictMode: true,
+  // Force all pages to render at request time (not at build time)
+  // This fixes the "Invalid URL" error when NEXTAUTH_URL is empty during build
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+}
+
 module.exports = nextConfig
